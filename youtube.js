4b939ea4892f1377ae8d6967c62e085f5bd0f0f8
christin-johny <christinjohnymankuthel@gmail.com>
2024-12-23 16:01:00 +0530

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,13 @@
+$('.sidebar').mCustomScrollbar({
+    
+});
+$('.chip-container').slick({
+    slidesToShow: 15,
+    slidesToScroll: 2,
+    arrows: true,
+    variableWidth: true,
+    prevArrow: '<div class="slick-prev"><img src="images/arrow_back.png" style="height:25px; flex-shrink:0;"></div>',
+    nextArrow: '<div class="slick-next"><img src="images/arrow_back.png" style="height:25px; transform: rotate(180deg); flex-shrink:0;"></div>'
+});
+
+
